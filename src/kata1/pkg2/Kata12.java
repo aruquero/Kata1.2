f7f90a94d410a07f8320739c6226f09e54a251fe
marco <marco@10.192.212.203>
2019-09-25 13:44:00 +0200

    kata1done

---
+++ src/kata1/pkg2/Kata12.java
@@ -0,0 +1,12 @@
+package kata1.pkg2;
+
+import java.util.Date;
+
+public class Kata12 {
+
+    public static void main(String[] args) {
+    person julio = new person("Pedro",new Date(16,8,1998));
+    System.out.println(julio);
+    }
+    
+}
